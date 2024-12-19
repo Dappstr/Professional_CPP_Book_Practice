7b8a56b0d08f315592f656b83f95f03187812ae6
Dappster <87837549+Dappstr@users.noreply.github.com>
2024-12-19 13:05:28 -0800

    Chapter 18

---
+++ Chapter 18/main.cpp
@@ -0,0 +1,6 @@
+int main() {
+    //std::vector<bool> v{}; The bool specialization for std::vector is more like a bitfield. Prefer std::bitset if using
+    // a set of bits which will not change in size, or use a vector of type `int_fast8_t` or unsigned char
+
+    return 0;
+}
